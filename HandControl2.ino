6ffeb17afb73e9ff5c7ab839337995f470578ef7
Chami Lamelas <sockz0101@gmail.com>
2018-04-12 19:22:58 +0100

    function & variable clean-up

--- HandControl2.ino
+++ HandControl2.ino
@@ -1,17 +1,14 @@
 #include <Servo.h>
-
-#include "ButtonAndLED.h"
 #include "Nunchuck.h"
 
-// *NOTE* these are servos for the device being operated by the nunchuck (i.e. the hand)
-// servos for the Pixy are controlled in ServoLoop using setServos() function Pixy class
 Servo fingersServo;
 Servo thumbServo;
 
 const short FINGERS_SERVO_PORT = 9;
 const short THUMB_SERVO_PORT = 10;
 
-int calcOutput(int curr, int dest) {
+// helper method to determine step used by moveHand() and functions like it if need be 
+int calcStep(int curr, int dest) {
   if (curr > dest) {
     if (curr < dest + 10)
       return curr - 1;
@@ -24,17 +21,18 @@ int calcOutput(int curr, int dest) {
   }
 }
 
-// gradual servo movement -> think this would prevent "drift"
+// gradual servo movement -> think this would prevent "drift", anyways it's what people seem to do
 void moveHand(long destFingers, long destThumb, int delayTime = 10) {
   int fingersPos = fingersServo.read(); 
   int thumbPos = thumbServo.read();
   while ((fingersPos < destFingers || fingersPos > destFingers) || (thumbPos < destThumb || thumbPos > destThumb)) {
-    fingersPos = calcOutput(fingersPos, destFingers);
-    thumbPos = calcOutput(thumbPos, destThumb);
+    fingersPos = calcStep(fingersPos, destFingers);
+    thumbPos = calcStep(thumbPos, destThumb);
     fingersServo.write(fingersPos);
     thumbServo.write(thumbPos);
     delay(delayTime);
 
+    // Servo debug stuff 
     Serial.print("--- Fingers ---");
     Serial.print(fingersPos);
     Serial.print("->");
@@ -52,56 +50,36 @@ void nunChuckLoop()
 {
   if (nunchuk_read()) {
 
-  // map nunchuk data to a servo data point
-    int x_axis = map(nunchuk_data[0], 23, 222, 180, 0);
-    int y_axis = map(nunchuk_data[1], 32, 231, 0, 180);
+  // map nunchuk data to a servo data point -> use for moving the arm 
+//    int x_axis = map(nunchuk_data[0], 23, 222, 180, 0);
+//    int y_axis = map(nunchuk_data[1], 32, 231, 0, 180);
   
     if (nunchuk_buttonC()) {
-      openHand();
+      Serial.print("Opening hand..");
+      moveHand(180, 90);
     }
     else if (nunchuk_buttonZ()) {
-      grab();
+      Serial.print("Closing hand..");
+      moveHand(0, 0);
     }
   // un-comment next line to print data to serial monitor  
    //nunchuk_print();          
    }
 }
 
-/*
- * function to open hand 
- */
-void openHand() {
-  Serial.print("Opening hand..");
-  moveHand(180, 90);
-}
-
-/*
- * function to grab
- */
-void grab() {
-  Serial.print("Closing hand..");
-  moveHand(0, 0);
-}
-
-void runAutonomous() { // use global variable "color" & use pantilt demo 
-  Serial.print("Running Autonomous on color: ");
-  Serial.print("Exiting Autonomous, handing back to nunChuckCycle()");
-}
-
 void setup() {
-    //pixy.init();
     Serial.print("Initiated set-up...\n");
     Serial.begin(9600);
     Wire.begin();
     fingersServo.attach(FINGERS_SERVO_PORT);
     thumbServo.attach(THUMB_SERVO_PORT);
-    setUpBtnAndLED();
     nunchuk_init();
     nunchuk_read();// get rid of c being pushed at start
     Serial.print("\nSet-up complete...\n");
 }
 void loop() {
   nunChuckLoop();
+// Nunchuck debug stuff 
 //  if (nunchuk_read()) 
 //    nunchuk_print();
 //  delay(10);
